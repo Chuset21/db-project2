a43a1487cb7cf18dc3134863b42c00f007e3c9ea
Miguel Bloggs <arrietam@tcd.ie>
2023-02-14 21:03:43 -0500

    Created player table and coach table

--- src/createtbl.sql
+++ src/createtbl.sql
@@ -6,11 +6,11 @@ connect to cs421;
 
 create table Match
 (
-    match_number integer     not null GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) not null,
+    match_number integer GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) not null,
     length       integer,
-    round        varchar(15) not null,
-    date         date        not null,
-    time         time        not null,
+    round        varchar(15)                                                        not null,
+    date         date                                                               not null,
+    time         time                                                               not null,
     primary key (match_number),
     constraint check_match check (length >= 0 and (round = 'final' or round = 'third place' or round = 'semifinals' or
                                                    round = 'quarterfinals' or round = 'round of 16' or
@@ -27,5 +27,29 @@ create table Team
     constraint check_team check (t_group = 'A' or t_group = 'B' or t_group = 'C' or t_group = 'D' or t_group = 'E' or
                                  t_group = 'F' or t_group = 'G' or t_group = 'H')
 );
+
+create table Coach
+(
+    c_role  varchar(30)                                                        not null,
+    name    varchar(30)                                                        not null,
+    dob     date                                                               not null,
+    cid     integer GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) not null,
+    country varchar(60)                                                        not null,
+    primary key (country, cid),
+    foreign key (country) references Team
+);
+
+create table Player
+(
+    position varchar(15)                                                        not null,
+    name     varchar(30)                                                        not null,
+    dob      date                                                               not null,
+    number   integer                                                            not null,
+    pid      integer GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) not null,
+    country  varchar(60)                                                        not null,
+    primary key (country, pid),
+    foreign key (country) references Team
+);
+
 
 
