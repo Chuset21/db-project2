229397142809fa3ebf6ea5ddd33577d46b3e9493
Miguel Bloggs <arrietam@tcd.ie>
2023-02-14 20:54:54 -0500

    Created table team and match

--- src/createtbl.sql
+++ src/createtbl.sql
@@ -2,18 +2,30 @@
 -- Make sure to terminate each statement with a semicolon (;)
 
 -- LEAVE this statement on. It is required to connect to your database.
-CONNECT TO cs421;
+connect to cs421;
 
--- Remember to put the create table ddls for the tables with foreign key references
---    ONLY AFTER the parent tables has already been created.
-
--- This is only an example of how you add create table ddls to this file.
---   You may remove it.
-CREATE TABLE MYTEST01
+create table Match
 (
-  id INTEGER NOT NULL
- ,value INTEGER
- ,PRIMARY KEY(id)
+    match_number integer     not null GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) not null,
+    length       integer,
+    round        varchar(15) not null,
+    date         date        not null,
+    time         time        not null,
+    primary key (match_number),
+    constraint check_match check (length >= 0 and (round = 'final' or round = 'third place' or round = 'semifinals' or
+                                                   round = 'quarterfinals' or round = 'round of 16' or
+                                                   round = 'group stage'))
+);
+
+create table Team
+(
+    national_association varchar(100)  not null,
+    url                  varchar(2083) not null,
+    t_group              char(1)       not null,
+    country              varchar(60)   not null,
+    primary key (country),
+    constraint check_team check (t_group = 'A' or t_group = 'B' or t_group = 'C' or t_group = 'D' or t_group = 'E' or
+                                 t_group = 'F' or t_group = 'G' or t_group = 'H')
 );
 
 
